a0f2159b788fb6c802924423a3610f4ac82b9bc0
Pawel Chmiel-Kozdranski <pch777@gazeta.pl>
2022-06-02 13:41:15 +0200

    create project with spring initializr

---
+++ src/main/java/com/pch777/blogs/BlogsAppApplication.java
@@ -0,0 +1,13 @@
+package com.pch777.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsAppApplication.class, args);
+	}
+
+}
